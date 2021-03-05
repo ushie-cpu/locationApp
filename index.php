4834558a25baeab6954033730fe1e2d03f749e2f
Ushie John <ushiejohn.uj@gmail.com>
2021-03-04 16:15:08 -0800

    locationApp

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <title></title>
+   <!--
+    Mobile Specific Metas
+    ==================================================
+    -->
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+   <!--
+    CSS
+    ==================================================
+    -->
+   <!-- Bootstrap-->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <!-- Animation-->
+   <link rel="stylesheet" href="css/animate.css">
+   <!-- Morris CSS -->
+   <link rel="stylesheet" href="css/morris.css">
+   <!-- FontAwesome-->
+   <link rel="stylesheet" href="css/font-awesome.min.css">
+   <!-- Icon font-->
+   <link rel="stylesheet" href="css/icon-font.css">
+   <!-- Owl Carousel-->
+   <link rel="stylesheet" href="css/owl.carousel.min.css">
+   <!-- Owl Theme -->
+   <link rel="stylesheet" href="css/owl.theme.default.min.css">
+   <!-- Colorbox-->
+   <link rel="stylesheet" href="css/colorbox.css">
+   <!-- Template styles-->
+   <link rel="stylesheet" href="css/style.css">
+   <!-- Responsive styles-->
+   <link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+
+   <div class="body-inner">
+   
+               <!--Content row end-->
+            </div>
+            <!--Container end-->
+         </div>
+         <!--Top bar end-->
+
+      </div>
+
+  
+      <!-- Banner area end-->
+      <section class="main-container" id="main-container">
+         <div class="container">
+            <div class="row text-center">
+               <div class="col-md-12">
+                  <h2 class="section-title"><span>check a location now</span>Location App</h2>
+               </div>
+            </div>
+          
+            <!-- Row End-->
+         </div>
+         <!-- container end-->
+         <div class="gap-60"></div>
+         <div class="ts-form" id="ts-form">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-6">
+                     <form class="contact-form" id="contact-form" action="" method="POST">
+                        <div class="error-container"></div>
+                        <div class="row">
+                           <div class="col-lg-12">
+                              <div class="form-group">
+                                 <input class="form-control form-name" id="startinglocation" name="startinglocation" placeholder="Starting Location" type="text" required="">
+                              </div>
+                           </div>
+                           <!-- Col end-->
+                           <div class="col-lg-12">
+                              <div class="form-group">
+                                 <input class="form-control form-website" id="dropoff" name="dropoff" placeholder="Drop-off point" type="url" required="">
+                              </div>
+                           </div>
+                         
+                        
+                        </div>
+                        <!-- Form row end-->
+                        <div class="text-left mt-5" style="float: right">
+                           <button class="btn btn-primary "type="submit">Submit</button>
+                        </div>
+                        <div class="text-left mt-5">
+                           <button class="btn btn-primary " type="Reset">Reset</button>
+                        </div>
+                     </form>
+                     <!-- Form end-->
+
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="map" >
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d31525.450403484447!2d7.453078499999992!3d9.001444400000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d9.080262099999999!2d7.4705943999999995!4m5!1s0x104e0b5c243dcb7d%3A0xdee905acaa3edaf7!2sfactory%20irisii%20address!3m2!1d9.036069!2d7.489513!5e0!3m2!1sen!2sng!4v1614902809862!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+
+      <div class="back-to-top affix" id="back-to-top" data-spy="affix" data-offset-top="10">
+         <button class="btn btn-primary" title="Back to Top"><i class="fa fa-angle-double-up"></i>
+            <!-- icon end-->
+         </button>
+         <!-- button end-->
+      </div>
+      <!-- End Back to Top-->
+
+      <!--
+      Javascript Files
+      ==================================================
+      -->
+      <!-- initialize jQuery Library-->
+      <script type="text/javascript" src="js/jquery.js"></script>
+      <!-- Popper-->
+      <script type="text/javascript" src="js/popper.min.js"></script>
+      <!-- Bootstrap jQuery-->
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+      <!-- Owl Carousel-->
+      <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+      <!-- Counter-->
+      <script type="text/javascript" src="js/jquery.counterup.min.js"></script>
+      <!-- Waypoints-->
+      <script type="text/javascript" src="js/waypoints.min.js"></script>
+      <!-- Color box-->
+      <script type="text/javascript" src="js/jquery.colorbox.js"></script>
+      <!-- Smoothscroll-->
+      <script type="text/javascript" src="js/smoothscroll.js"></script>
+      <!-- Google Map API Key-->
+      <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCsa2Mi2HqyEcEnM1urFSIGEpvualYjwwM"></script>
+      <!-- Google Map Plugin-->
+      <script type="text/javascript" src="js/gmap3.js"></script>
+      <!-- Template custom-->
+      <script type="text/javascript" src="js/custom.js"></script>
+   </div>
+   <!--Body Inner end-->
+</body>
+</html>
